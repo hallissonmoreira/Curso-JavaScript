4fe108207e8e8a8fbd5834c20d377bad8a96207a
Hallisson Moreira <95989694+hallissonmoreira@users.noreply.github.com>
2021-12-13 19:43:22 -0300

    Atualizei o diretório

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 55;
+console.log(`A velocidade do seu carro é ${vel}km/h.`);
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade máxima permitida. Você foi MULTADO!`);
+}
+console.log(`Dirija sempre usando o cinto de segurança.`);
