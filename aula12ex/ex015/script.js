f46954966105a6ccbe77bd9127eeb53ed0989954
Hallisson Moreira <95989694+hallissonmoreira@users.noreply.github.com>
2021-12-14 12:46:39 -0300

    Atualizei a aula 12

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,41 @@
+function verificar() {
+    let ano = document.getElementById("txtano");
+    let result = document.getElementById("res");
+    let data = new Date();
+    let nascimento = Number(ano.value);
+    let idade = data.getFullYear() - nascimento;
+    let sexo = document.getElementsByName('txtsex');
+    let img = document.createElement("img");
+    img.setAttribute('id', 'foto')
+    // verificando o sexo.
+    if (sexo[0].checked) {
+        if (idade >=0 && idade < 12) {
+            img.setAttribute('src', 'imagens/manC.png');
+        } else if (idade < 21) {
+            img.setAttribute('src', 'imagens/manY.png');
+        } else if (idade < 60) {
+            img.setAttribute('src', 'imagens/manA.png');
+        } else {
+            img.setAttribute('src', 'imagens/manO.png');
+        }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Você é um homem de ${idade} anos.`
+        res.appendChild(img);
+    } else {
+        if (idade >=0 && idade < 12) {
+            img.setAttribute('src', 'imagens/womanC.png');
+        } else if (idade < 21) {
+            img.setAttribute('src', 'imagens/womanY.png');
+        } else if (idade < 60) {
+            img.setAttribute('src', 'imagens/womanA.png');
+        } else {
+            img.setAttribute('src', 'imagens/womanO.png');
+        }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Você é uma mulher de ${idade} anos.`
+        res.appendChild(img);
+    }
+    //verificando a idade
+
+
+}
